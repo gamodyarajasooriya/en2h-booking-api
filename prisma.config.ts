baffbf5e2582ea9b5c32b115f7a92737216860bd
Gamodya Rajasooriya <gamodyarajasooriya@gmail.com>
2026-07-11 19:15:28 +0530

    feat: add Prisma seed script

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
+    seed: "ts-node prisma/seed.ts",
   },
   datasource: {
     url: env("DATABASE_URL"), // Database URL is loaded from environment variables here
